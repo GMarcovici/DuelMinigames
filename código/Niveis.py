1c1fdb910a6f4b583f676cf64042611caa5d9e2e
Gabriellemos11 <gabrielagl@al.insper.edu.br>
2025-05-27 21:32:08 -0300

    Level4

--- código/Niveis.py
+++ código/Niveis.py
@@ -1,7 +1,7 @@
 #Inicialização --------------------------------------------------------------------------------------------------------------
 import pygame, random, math, classes, Functions, LoopPrincipal #bibliotecas
 # Tela
-
+from Functions import draw_text
 pygame.init()
 WIDTH, HEIGHT = 1400, 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -309,4 +309,116 @@ def level_3(player1, player2):
         pygame.display.flip()
         clock.tick(FPS)
     
+    return 1 if player2.lives <= 0 else 2 if player1.lives <= 0 else 0
+
+def level_4(player1, player2):
+    #Posição dos jogadores e direção inicial
+    player1.rect.x = WIDTH // 4 - player1.rect.width // 2
+    player1.rect.y = HEIGHT // 2 - player1.rect.height // 2
+    player1.direction = "right"
+    player1.image = player1.images["right"]
+    player2.rect.x = 3 * WIDTH // 4 - player2.rect.width // 2
+    player2.rect.y = HEIGHT // 2 - player2.rect.height // 2
+    player2.direction = "left"
+    player2.image = player2.images["left"]
+    
+    square_size = 50
+    square = pygame.Rect(WIDTH*0.5 - square_size*1.7, HEIGHT*0.5 - square_size*3.5, square_size, square_size)
+    square_color = RED
+    square_change_time = random.randint(2, 4) * FPS
+    square_timer = 0
+    can_shoot = False#Não pode atirar até o farol ficar verde
+    winner = 0
+    shot_fired = False#Não pode atirar até o farol ficar verde
+    
+    #Define a imagem do farol verde e vermelho
+    green_square = pygame.image.load('Assets/Objetos/farolverde-1.png.png').convert_alpha()
+    green_square = pygame.transform.scale(green_square, (150, 150))
+    red_square = pygame.image.load('Assets/Objetos/farolvermelho-1.png.png').convert_alpha()
+    red_square = pygame.transform.scale(red_square, (150, 150))
+
+    #Plano de fundo
+    FundoLevel4 = pygame.image.load('Assets/Background/fundodeserto.png').convert()
+    FundoLevel4 = pygame.transform.scale(FundoLevel4, (WIDTH*1.09, HEIGHT*1.7))
+
+    #Loop do nível 4
+    running = True
+    while running:
+        #Desenha a tela de fundo
+        screen.fill(BLACK)
+        screen.blit(FundoLevel4, (-WIDTH*0.03, -HEIGHT*0.4))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 0
+            
+            if can_shoot and not shot_fired and event.type == pygame.KEYDOWN:
+                if event.key == player1.shoot_key:
+                    player1.shoot()
+                    shot_fired = True
+                
+                if event.key == player2.shoot_key:
+                    player2.shoot()
+                    shot_fired = True
+        
+        square_timer += 1
+        if square_timer >= square_change_time:
+            #Altera o farol vermelho pelo verde e permite atirar
+            if square_color == RED:
+                square_color = GREEN
+                can_shoot = True
+                shot_fired = False
+                square_change_time = 1 * FPS
+            #Altera o farol verde pelo vermelho e não permite atirar
+            else:
+                square_color = RED
+                can_shoot = False
+                square_change_time = random.randint(2, 4) * FPS
+            square_timer = 0
+        
+        #Atualiza os tiros
+        player1.update_bullets([], None, player2)
+        player2.update_bullets([], None, player1)
+        
+        #Checa colisão dos tiros com os jogadores
+        if shot_fired:
+            for bullet in player1.bullets:
+                if bullet.rect.colliderect(player2.rect):
+                    winner = 1
+                    running = False
+            
+            for bullet in player2.bullets:
+                if bullet.rect.colliderect(player1.rect):
+                    winner = 2
+                    running = False
+        
+        if player1.lives <= 0 or player2.lives <= 0:
+            running = False
+        
+        #Desenha os jogadores e tiros
+        screen.blit(player1.image, player1.rect)
+        screen.blit(player2.image, player2.rect)
+        player1.bullets.draw(screen)
+        player2.bullets.draw(screen)
+        
+        player1.draw_lives(screen)
+        player2.draw_lives(screen)
+        
+        #Desenha os faróis
+        if square_color == GREEN:
+            pygame.mixer.Sound('Assets/Sounds/hit.wav').play()#som se o farol ficar verde
+            screen.blit(green_square, square)
+        else:
+            screen.blit(red_square, square)
+        
+        if can_shoot and not shot_fired:
+            draw_text("ATIRE AGORA!", font, WHITE, WIDTH//2, HEIGHT//2 + 70, screen)
+        
+        draw_text(f"J1 : {player1.score}", font, BLUE, WIDTH*0.25, HEIGHT*0.02, screen)
+        draw_text(f"J2 - {player2.score}", font, RED, WIDTH*0.75, HEIGHT*0.02, screen)
+        
+        pygame.display.flip()
+        clock.tick(FPS)
+    
+    #retorna o vencedor
     return 1 if player2.lives <= 0 else 2 if player1.lives <= 0 else 0
